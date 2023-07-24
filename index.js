bf4fa9d6b5752218b5f634f9705c010013491b27
Masumraihan <mdmasumraihan1@gamil.com>
2023-07-24 11:57:12 +0600

    second class

---
+++ index.js
@@ -0,0 +1,40 @@
+//var num1 = .2;
+//var num2 = .1;
+//const sum = num1+num2;
+//console.log(sum.toFixed(2))
+//console.log(Math.round(sum));
+//console.log(Math.floor(sum));
+//console.log(Math.ceil(sum));
+
+//var arr = [2,"masum",{Roll:'student'}]
+//var obj = {Roll:'student'}
+//var obj2 = arr[2].Roll
+//console.log(arr.indexOf("masum"));
+
+//var array = [2,3,5,64,43,23,523];
+//array.pop()
+//console.log(array);
+//console.log(array2);
+
+//var array2 = [2,3,5,64,43,23,523];
+//array2.push("masum",2)
+//console.log(array2);
+
+//var array3 = [2,3,5,64,43,23,523];
+//array3.unshift(53,5)
+//console.log(array3);
+
+//var array4 = [2,3,5,64,43,23,523];
+//array4.shift()
+//console.log(array4);
+
+//var arr = [2,3,5,64,43,23,523];
+//arr.splice(2,3)
+//console.log("splice",arr);
+
+//var array = [2,3,5,64,43,23,523];
+// var arr2 = array.slice(2,4)
+//console.log(arr2);
+//console.log(array);
+
+//var student = {name:"masum", roll:2, isHappy: true }
