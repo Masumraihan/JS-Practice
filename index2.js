f756b2c29d1065c2fccb56dd4d60b1edecaadb3e
Masumraihan <mdmasumraihan1@gamil.com>
2023-07-29 01:38:51 +0600

    update

--- index2.js
+++ index2.js
@@ -83,8 +83,8 @@
 //    return n + n2
 //}
 
-const myFn2 =  (n,n2,n3) => n+n2+n3;
-console.log(myFn2(2,3));
+//const myFn2 =  (n,n2,n3) => n+n2+n3;
+//console.log(myFn2(2,3));
 
 
 //let k = 2;
@@ -93,3 +93,110 @@ console.log(myFn2(2,3));
 // const n = 3;
 // n = 3
 //console.log(n);
+
+//function fn1 (p) {
+//return p * k
+//}
+
+//for (let i = 0; i < 20; i++) {
+//    const element = arr[i];
+//}
+
+//const arr = [2,34,234,523,42];
+
+//for (const num of arr) {
+//    console.log(num);
+//}
+
+//const obj = {name:"masum", number:5,status: true};
+
+//for (const key in obj) {
+//    console.log(obj[key]);
+//}
+//const key = "name"
+//console.log(obj[key]);
+
+//const obj = {name:"masum", number:5,status: true};
+//const keys = Object.keys(obj)
+//const values = Object.values(obj)
+//console.log(values);
+
+// const forEach = keys.forEach(key =>{
+//    console.log(obj[key]);
+//})
+//console.log(forEach);
+
+//const arr = [2,34,234,523,42];
+
+//const map = arr.map((n,i,newArr) =>{
+//    console.log("n",n);
+//    console.log("i",i);
+//    console.log("aar",newArr);
+//});
+//console.log(map);
+
+//const newArr = [];
+//arr.forEach( n => {
+//    const newN = n - 1;
+//    newArr.push(newN)
+//})
+//console.log(newArr);
+
+//const arr2 = ["masum", "belal", "raihan","mr"];
+
+//const filter = arr.filter(n => n === 7);
+//const names = arr.filter(n => n !== "34")
+//console.log(names);
+
+//const name = arr2.find(n => n === "m")
+//console.log(name);
+
+//const srt = "this is pen on a table which is dark on green";
+//function reverse(srt) {
+//    const arr = srt.split("");
+//    let string = "";
+//    for (let i = arr.length - 1; i >= 0; i--) {
+//        const element = arr[i];
+//        string += element
+//    }
+//    console.log(string);
+//}
+//reverse(srt)
+
+//const findMiddle = (str) => {
+//    const arr = str.split(" ");
+//    const map = arr.map((word,i,arr) => {
+//        if (word === "is" || word === "on") {
+//            if (word === "is") {
+//                console.log(arr[++i]);
+//            }
+//        }
+//    });
+//}
+//findMiddle(srt)
+
+//let arr3 = [3,43,5423,234,523,3];
+//let newArray = [...arr3]
+//newArray.push(33333)
+//console.log(newArray);
+//console.log(arr3);
+
+//let obj = {Name:"masum",Roll:3,address:"natore"};
+//let newObj = {...obj};
+//newObj["Name"] = "raihan";
+//console.log(obj);
+
+//let n = 3;
+//if (2>1) {
+//    let n =3 
+//    console.log(n);
+//}
+//const num = 43
+//const newSrt = `My name is Masum Raihan. ${num + 1} I am from Natore`;
+//console.log(newSrt);
+
+//const n1 = 3;
+//const value = n1 === 3 ? "rich" : "poor";
+//const value2 = n1 === 2 || "rich"
+//const value3 = n1 === 2 && "rich"
+//console.log(value2); 
